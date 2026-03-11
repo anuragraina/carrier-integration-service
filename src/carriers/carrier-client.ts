31ee18563674ca56134f95f211ced457cae39e84
Anurag Raina <rainakalhan90@gmail.com>
2026-03-11 13:32:14 +0530

    Add carrier abstraction for extensible integrations

---
+++ src/carriers/carrier-client.ts
@@ -0,0 +1,6 @@
+import type { RateQuote } from "../domain/rate-quote.js";
+import type { RateRequest } from "../domain/rate-request.js";
+
+export interface CarrierClient {
+  getRates(request: RateRequest): Promise<RateQuote[]>;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { loadConfig } from './config/env.js';
+export type { CarrierClient } from './carriers/carrier-client.js';
+export type { RateRequest } from './domain/rate-request.js';
+export type { RateQuote } from './domain/rate-quote.js';
 export * from './domain/errors.js';
